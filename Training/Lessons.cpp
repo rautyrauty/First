8f3606b623059c445a246e7407e2d5b6e50b8bd6
Rautyst <108220148+Rautyst@users.noreply.github.com>
2022-06-26 23:54:08 +0500

    Добавил комментарий в бинарный поиск границ массива

--- Training/Lessons.cpp
+++ Training/Lessons.cpp
@@ -41,7 +41,7 @@ int binright_boundary(T x, const vector<T>& A)
 }
 void binSearch_for_boundaries_in_vector()
 {
-	T x; vector<T> A = { 2,2,2,3,3,3,5,5,5,5,5,6,7,7,9,9 }; 
+	T x; vector<T> A = { 2,2,2,3,3,3,5,5,5,5,5,6,7,7,9,9 }; // Объявление значения для поиска и массива для поиска
 	cout << "What to find? x = "; cin >> x;
 	int left = left_boundary(x, A); int right = right_boundary(x, A);
 	cout << "Noob finder give answer: "
