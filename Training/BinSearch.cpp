3f47019660e7f00f8215ba7bc857ced623592b11
Rautyst <108220148+Rautyst@users.noreply.github.com>
2022-06-26 21:05:42 +0500

    Разложил по воидам все коды
    Теперь можно хранить все коды в воидах и при необходимости просто вызывать их! Ура!

--- Training/BinSearch.cpp
+++ Training/BinSearch.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-//#include <iomanip>
+#include <iomanip>
 using namespace std;
 typedef double T;
+// Бинарный поиск границ в массиве
 int left_boundary(T x, const vector<T>& A)
 {
 	int left = 0;
@@ -12,13 +13,13 @@ int left_boundary(T x, const vector<T>& A)
 }
 int right_boundary(T x, const vector<T>& A)
 {
-	int right = A.size()-1;
+	int right = int(A.size())-1;
 	while (right > 0 and A[right] > x) right--;
 	return right;
 }
 int binleft_boundary(T x, const vector<T>& A)
 {
-	int l = 0, r = A.size()-1;
+	int l = 0, r = int(A.size())-1;
 	while (r - l > 1)
 	{
 		int middle = (r + l) / 2;
@@ -29,7 +30,7 @@ int binleft_boundary(T x, const vector<T>& A)
 }
 int binright_boundary(T x, const vector<T>& A)
 {
-	int l = 0, r = A.size() - 1;
+	int l = 0, r = int(A.size()) - 1;
 	while (r - l > 1)
 	{
 		int middle = (r + l) / 2;
@@ -38,7 +39,7 @@ int binright_boundary(T x, const vector<T>& A)
 	}
 	return r;
 }
-void binSearch()
+void binSearch_for_boundaries_in_vector()
 {
 	T x; vector<T> A = { 2,2,2,3,3,3,5,5,5,5,5,6,7,7,9,9 }; // Я самый крутой человек на свете
 	cout << "What to find? x = "; cin >> x;
@@ -48,4 +49,164 @@ void binSearch()
 	int binleft = binleft_boundary(x, A); int binright = binright_boundary(x, A);
 	cout << "Binsearch finder give answer: "
 		<< '[' << left << ';' << right << ']' << '\n';
+}
+//Бинарный поиск корня функции f(x)
+double f(double x) // Бинарный поиск корня функции с погрешностью
+{
+	return (x - 2.21313) * (x - 7);
+}
+void binSearch_root()
+{
+	double a, b; cout << "Enter boundaries [a,b]: "; cin >> a >> b;
+	double ERROR; cout << "Enter error: "; cin >> ERROR;
+	if (f(a) * f(b) < 0) {
+		while ((b - a) / 2. > ERROR) {
+			double c = (a + b) / 2;
+			if (f(c) == 0) {
+				cout << "OMG!!! 1001% answer is: " << c << '\n';
+			}
+			if (f(a) * f(c) <= 0) b = c;
+			else a = c;
+		}
+		cout << "Answer is: " << setprecision(17) << (a + b) / 2 << '\n';
+	}
+	else { cout << "Cant use binsearch of root bcs f(a) * f(b) >= 0\n"; }
+}
+//Действия с векторами
+void print_vector(const vector<int>& A)
+{
+	for (int i = 0; i < int(A.size()); i++)
+	{
+		cout << A[i] << ',';
+	}
+	cout << '\n';
+}
+void shift_right(vector<int>& A) // Сдвиг вектора вправо
+{
+	int x = A[A.size()-1];
+	for (int i =int(A.size()) - 1; i > 0; i--)
+	{
+		A[i] = A[i - 1];
+	}
+	A[0] = x;
+}
+void shift_left(vector<int>& A) // Сдвиг вектора влево
+{
+	int x = A[0];
+	for (int i = 0; i < int(A.size())-1; i++)
+	{
+		A[i] = A[i + 1];
+	}
+	A[A.size()-1] = x;
+}
+void reverse(vector<int>& A) // Поменять значения вектора задом наперёд
+{
+	int N = int(A.size());
+	for (int i = 0; i <= N/2; i++)
+	{
+		int x = A[i];
+		A[i] = A[N - i - 1];
+		A[N - i - 1] = x;
+	}
+}
+//Пример решение 27 задания ЕГЭ со змейкой
+void zmeyka27() { // Решение задачи 27 с идеей со змейкой
+	const int Q_size = 5;
+	int Q[Q_size];
+	for (int i = 0; i < Q_size; i++)
+	{
+		cin >> Q[i];
+	}
+	int Q_end = 0, max_from_before = -1, N = 15, max_sum = -1;
+	for (int i = Q_size; i < N; i++)
+	{
+		int current, number_leaving_queue = Q[Q_end];
+		cin >> current;
+		Q[Q_end++] = current;
+		if (max_sum < current + max_from_before) max_sum = current + max_from_before;
+		if (Q_end == Q_size) Q_end = 0;
+		if (number_leaving_queue > max_from_before) max_from_before = number_leaving_queue;
+	}
+	cout << max_sum << '\n';
+}
+//Попытка в структуры и новый оператор
+//Задача с поиском ЛЮДЕЙ с максимальным возрастом
+struct Person {
+	int age;
+	string name;
+};
+ostream& operator << (ostream& out, Person& one) {
+	out << '[' << one.name << ']';
+	return out;
+}
+void poisk()
+{
+	int N; cin >> N;
+	vector<int> ages_count(101);
+	vector<Person> crowd(N);
+	for (int i = 0; i < N; i++)
+	{
+		Person one;
+		cin >> one.name >> one.age;
+		crowd[i] = one;
+		ages_count[one.age]++;
+	}
+	int max_count = -1, max_age=0;
+	for (int i = 0; i < 101; i++)
+	{
+		if (ages_count[i] > max_count) {
+			max_count = ages_count[i];
+			max_age = i;
+		}
+		//cout << ages_count[i] << '\n';
+	}
+	for (int i = 0; i < N; i++) {
+		if (crowd[i].age == max_age) cout << crowd[i] << '\n';
+	}
+}
+//Задача с проверкой правильности написания разных видов скобок в матем. выражении 
+void skobki() // Задача про разные виды скобок
+{
+	string a; vector<char> A(10);int THE_END = 0; // Задание про СТЕК, объявление строки, стека и конца стека соответственно
+	cout << "Enter the virajenie: ";cin >> a; // Ввод выражения со скобками(или без)
+	bool f = 1; //флаг для определения корректности последовательности введёных скобок
+	for (int i = 0; a[i] != '\0'; i++)
+	{
+		switch (a[i])
+		{
+		case '[':
+			A[THE_END++] = a[i];
+			break;
+		case '(':
+			A[THE_END++] = a[i];
+			break;
+		case '{':
+			A[THE_END++] = a[i];
+			break;
+		case ')':
+			if (A[THE_END - 1] == '(') { A[--THE_END] = '0'; }
+			else { f = 0; }
+			break;
+		case ']':
+			if (A[THE_END - 1] == '[') {A[--THE_END] = '0';}
+			else { f = 0; }
+			break;
+		case '}':
+			if (A[THE_END - 1] == '[') { A[--THE_END] = '0'; }
+			else { f = 0; }
+			break;
+		default:
+			break;
+		}
+		if (f == 0) {
+			cout << "\nERROR SYNTAX ";
+			break;
+		}
+	}
+	if (THE_END != 0) { cout << "ERROR\n"; }
+	else { cout << "\nCORRECT\n"; }
+}
+int main()
+{
+	return 0;
 }
